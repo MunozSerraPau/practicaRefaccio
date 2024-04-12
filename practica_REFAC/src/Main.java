bb0d7af6d9b4024d36c2a8d6bdc4f71f82c170db
Pau Muñoz Serra <147044090+MunozSerraPau@users.noreply.github.com>
2024-04-12 11:14:54 +0200

    pROA

--- practica_REFAC/src/Main.java
+++ practica_REFAC/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.List;
 import java.util.Scanner;
 //hola
+// klk
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
