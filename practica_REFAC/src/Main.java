21a6328fa924634ba50a29d81e96fb438b003050
albam <albamamo07@gmail.com>
2024-04-26 09:44:50 +0200

    D -> Discriminant

--- practica_REFAC/src/Main.java
+++ practica_REFAC/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.List;
 import java.util.Scanner;
 public class Main {
+    //hola
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String p = "Proves";
