61ff73a22a859ea9457bdd1d6e7775d3ffc4787a
albam <albamamo07@gmail.com>
2024-05-15 17:41:18 +0200

    Añadir comentarios y añadir el tipo de REFACT

--- practica_REFAC/src/Main.java
+++ practica_REFAC/src/Main.java
@@ -10,16 +10,17 @@ public class Main {
 
         //REFACT 11
         String opcio;
+        //Modifiquem el tipus de la variable opció per no tenir cap problema a l'hora d'introduir altres dades incorrectes.
         do {
-            //REFACT 1
+            //REFACT 1 Tipus: Extracció de mètode
             menu();
-            //
+            //Hem creat una funció pròpia per al menú on el mostrem un cop la cridem.
             opcio = scan.nextLine();
             switch (opcio) {
                 case "1":
-                    //REFACT 2
+                    //REFACT 2 Tipus: Extracció de mètode
                     compararNum();
-                    //
+                    //Hem canviat el codi de lloc i hem creat un mètode amb ell.
                     break;
                 case "2":
                     double a=2;
@@ -29,32 +30,33 @@ public class Main {
                     break;
                 case "3":
                     List<OrderLineItem> lineItems = null;
-                    //REFACT 9
+                    //REFACT 9 Tipus: Reanomenar
                     Order item = new Order(lineItems, 5.5);
-                    //
+                    //Modifiquem el nom de la variable per fer-la una mica més entenedora.
                     break;
                 case "0":
                     break;
                 default:
                     //REFACT 3
                     System.out.println("ATENCIÓ!!! \nHa de ser un valor entre 0 i 3");
-                    //
+                    //El menú només conte 3 punts (abans 0 i 5).
             }
         } while (opcio.equals("0"));
     }
 
-    //REFACT 1.1
+    //REFACT 1.1 Tipus: Extracció de mètode
+    //Aquest és el codi que es trobava a la funció Main.
     public static void menu() {
         System.out.println("1. Comparar dos números");
         System.out.println("2. Càlcul equació de segon grau");
         System.out.println("3. Calcular preu");
         //REFACT 8
-        //
+        //Esborrem els prints innecessaris, ja que només tenim 3 punts.
         System.out.println("0. Acabar");
     }
-    //
 
-    //REFACT 2.1
+    //REFACT 2.1 Tipus: Extracció de mètode
+    //Hem fet una nova funció amb el codi que es trobava al switch.
     public static void compararNum () {
         int num1 = obtenirNum();
         int num2 = obtenirNum();
@@ -68,7 +70,8 @@ public class Main {
         }
     }
 
-    //REFACT 2.2
+    //REFACT 2.2 Tipus: Extracció de mètode, Substitució d’algoritme(Evitem que es repeteixi demanar el número de forma repetitiva.)
+    //També hem fet una funció per demanar els valors i gestionar les excepcions.
     public static int obtenirNum() throws IllegalArgumentException{
         System.out.println("Introdueix un numero: ");
         int num = 0;
@@ -94,20 +97,20 @@ public class Main {
     }
 
     public static void calcEquacioSegongrau(double a, double b, double c) {
-        //REFACT 4
+        //REFACT 4 Tipus: Reanomenar
         double Discriminant = b * b - 4 * a * c;
-        //
+        //Canviem d per → Discriminant per fer el codi més entenedor.
 
         if (Discriminant > 0) {
             double x1, x2;
-            //REFACT 5
+            //REFACT 5 Tipus: Extracció de mètode
             x1 = calculOperacio2nGr('-', a, b, Discriminant);
             x2 = calculOperacio2nGr('+', a, b, Discriminant);
-            //
+            //Fem una nova funció per fer els càlculs de x1 i x2
 
-            //REFACT 6
+            //REFACT 6 Tipus: Reanomenar
             System.out.println("X1 és igual a = " + x1 + ", X2 és igual a = " + x2);
-            //
+            //Modifiquem el print per fer-lo més entenedor.
 
         } else if (Discriminant == 0) {
             double x;
@@ -115,13 +118,14 @@ public class Main {
 
             System.out.println("X és igual a = " + x);
         } else {
-            //REFACT 7
+            //REFACT 7 Tipus: Reanomenar
             System.out.println("L'equació no té arrels reals.");
-            //
+            //Modifiquem el print per fer-lo més entenedor.
         }
     }
 
-    //REFACT 5.1
+    //REFACT 5.1 Tipus: Extracció de mètode
+    //Aquesta és la nova funció amb la qual fem els càlculs necessaris per obtenir el valor de x1 i x2.
     public static double calculOperacio2nGr (char t, double a, double b, double D) {
 
         if (t == '+') {
@@ -132,7 +136,8 @@ public class Main {
         return 0;
     }
 
-    //REFACT 5.2
+    //REFACT 5.2 Tipus: Extracció de mètode
+    //Aquesta és la nova funció amb la qual fem els càlculs necessaris per obtenir el valor de x.
     public static double calculOperacio2nGrSimple(double a, double b) {
         return -b / (2 * a);
     }
