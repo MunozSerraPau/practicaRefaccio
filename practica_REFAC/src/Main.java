f11f6ee1d912e59796b5bdd6e4de498396be728f
albam <albamamo07@gmail.com>
2024-05-15 16:29:38 +0200

    AÃñadir etiquetas REFACT en las modificaciones

--- practica_REFAC/src/Main.java
+++ practica_REFAC/src/Main.java
@@ -1,22 +1,25 @@
 import java.io.File;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-
         String p = "Proves";
 
         int opcio;
         do {
+            //REFACT 1
             menu();
-
+            //
             opcio = scan.nextInt();
             scan.nextLine();
             switch (opcio) {
                 case 1:
+                    //REFACT 2
                     compararNum();
+                    //
                     break;
                 case 2:
                     double a=2;
@@ -26,22 +29,21 @@ public class Main {
                     break;
                 case 3:
                     List<OrderLineItem> lineItems = null;
+                    //REFACT
                     Order asd = new Order(lineItems, 5.5);
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
+                    //
                     break;
                 case 0:
                     break;
                 default:
-                    System.out.println("ATENCIÓ!!! \nHa de ser un valor entre 0 i 5");
+                    //REFACT 3
+                    System.out.println("ATENCIÓ!!! \nHa de ser un valor entre 0 i 3");
+                    //
             }
         } while (opcio != 0);
     }
 
+    //REFACT 1.1
     public static void menu() {
         System.out.println("1. ");
         System.out.println("2. ");
@@ -50,8 +52,9 @@ public class Main {
         System.out.println("5. ");
         System.out.println("0. Acabar");
     }
+    //
 
-
+    //REFACT 2.1
     public static void compararNum () {
         int num1 = obtenirNum();
         int num2 = obtenirNum();
@@ -59,11 +62,13 @@ public class Main {
         if (num1 == num2) {
             System.out.println("Els dos números son iguals.");
         } else if (max(num1,num2)) {
-            System.out.println("El numero gran és: " + num1);
+            System.out.println("El número més gran és: " + num1);
         } else {
-            System.out.println("El numero gran és: " + num2);
+            System.out.println("El número més gran és: " + num2);
         }
     }
+
+    //REFACT 2.2
     public static int obtenirNum() {
         int num;
 
@@ -71,13 +76,15 @@ public class Main {
         try {
             num = scan.nextInt();
             scan.nextLine();
-        } catch (Exception e) {
+
+        } catch (InputMismatchException e) {
             System.out.println("ERROR! Ha de ser un INT, se li ha assignat el valor 5.");
             num = 5;
         }
 
         return num;
     }
+
     public static boolean max(int a, int b) {
         if(a > b) {
             return true;
@@ -88,24 +95,35 @@ public class Main {
         }
     }
 
-    //
     public static void calcEquacioSegongrau(double a, double b, double c) {
+        //REFACT 4
         double Discriminant = b * b - 4 * a * c;
-      
+        //
+
         if (Discriminant > 0) {
             double x1, x2;
+            //REFACT 5
             x1 = calculOperacio2nGr('-', a, b, Discriminant);
             x2 = calculOperacio2nGr('+', a, b, Discriminant);
-            System.out.println("Resposta numero 1 = " + x1 + ", Resposta numero 2 = " + x2);
+            //
+
+            //REFACT 6
+            System.out.println("X1 és igual a = " + x1 + ", X2 és igual a = " + x2);
+            //
+
         } else if (Discriminant == 0) {
             double x;
             x = calculOperacio2nGrSimple(a, b);
 
-            System.out.println("L'única resposta és = " + x);
+            System.out.println("X és igual a = " + x);
         } else {
-            System.out.println("L'equació no te cap resposta.");
+            //REFACT 7
+            System.out.println("L'equació no té arrels reals.");
+            //
         }
     }
+
+    //REFACT 5.1
     public static double calculOperacio2nGr (char t, double a, double b, double D) {
 
         if (t == '+') {
@@ -115,6 +133,8 @@ public class Main {
         }
         return 0;
     }
+
+    //REFACT 5.2
     public static double calculOperacio2nGrSimple(double a, double b) {
         return -b / (2 * a);
     }
@@ -179,6 +199,7 @@ public class Main {
         }
     }
 
+    //La clase Customer no s'utilitza mai
     public class Customer {
         private String firstName;
         private String lastName;
